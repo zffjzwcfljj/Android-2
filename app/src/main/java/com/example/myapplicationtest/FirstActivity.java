55a6770d39279521877c4343629cccdf312ea5f5
zffjzwcfljj <1601950538@qq.com>
2019-09-16 16:00:46 +0800

    add menu

--- app/src/main/java/com/example/myapplicationtest/FirstActivity.java
+++ app/src/main/java/com/example/myapplicationtest/FirstActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplicationtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -22,5 +24,23 @@ public class FirstActivity extends AppCompatActivity {
                         Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this,"You clicked Add",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this,"You clicked Remove",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
     }
 }
